4b2723823b30f06bff2c87be1d834bf076a052d2
Robert Maguire <LazyWizard@users.noreply.github.com>
2014-07-03 22:15:40 -0500

    Implemented missile renderer

--- org/lazywizard/radar/renderers/MissileRenderer.java
+++ org/lazywizard/radar/renderers/MissileRenderer.java
@@ -1,24 +1,108 @@
 package org.lazywizard.radar.renderers;
 
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.CombatEntityAPI;
+import com.fs.starfarer.api.combat.GuidedMissileAI;
+import com.fs.starfarer.api.combat.MissileAIPlugin;
+import com.fs.starfarer.api.combat.MissileAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import java.awt.Color;
+import java.util.List;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.lazywizard.lazylib.JSONUtils;
+import static org.lazywizard.lazylib.opengl.ColorUtils.glColor;
 import org.lazywizard.radar.BaseRenderer;
 import org.lazywizard.radar.RadarInfo;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.vector.Vector2f;
 
 public class MissileRenderer implements BaseRenderer
 {
+    private static boolean SHOW_MISSILES;
+    private static boolean SHOW_MISSILE_LOCK_ICON;
+    private static Color MISSILE_LOCKED_COLOR;
+    private RadarInfo radar;
+
     @Override
     public void reloadSettings(JSONObject settings, boolean useVanillaColors) throws JSONException
     {
+        SHOW_MISSILES = settings.getBoolean("showMissiles");
+        SHOW_MISSILE_LOCK_ICON = settings.getBoolean("showMissileLockIcon");
+        MISSILE_LOCKED_COLOR = JSONUtils.toColor(settings.getJSONArray("lockedMissileColor"));
     }
 
     @Override
     public void init(RadarInfo radar)
     {
+        this.radar = radar;
     }
 
     @Override
     public void render(float amount)
     {
+        ShipAPI player = radar.getPlayer();
+        if (SHOW_MISSILES && !player.isHulk())
+        {
+            List<? extends CombatEntityAPI> missiles = radar.filterVisible(
+                    Global.getCombatEngine().getMissiles());
+            if (!missiles.isEmpty())
+            {
+                boolean playerLock = false;
+
+                glPointSize(2f);
+                glBegin(GL_POINTS);
+                for (CombatEntityAPI entity : missiles)
+                {
+                    MissileAPI missile = (MissileAPI) entity;
+                    // TODO: Add a setting for missile damage alpha mod
+                    float alphaMod = Math.min(1f, Math.max(0.3f,
+                            missile.getDamageAmount() / 750f));
+                    alphaMod *= (missile.isFading() ? .5f : 1f);
+
+                    // Burnt-out missiles count as hostile
+                    if (missile.isFizzling())
+                    {
+                        glColor(radar.getEnemyContactColor(),
+                                radar.getContactAlpha() * alphaMod, false);
+                    }
+
+                    // Enemy missiles
+                    else if (missile.getOwner() + player.getOwner() == 1)
+                    {
+                        // Color missiles locked onto us differently
+                        MissileAIPlugin ai = missile.getMissileAI();
+                        if (ai != null && ai instanceof GuidedMissileAI
+                                && player == ((GuidedMissileAI) ai).getTarget())
+                        {
+                            playerLock = true;
+                            glColor(MISSILE_LOCKED_COLOR,
+                                    radar.getContactAlpha() * alphaMod, false);
+                        }
+                        else
+                        {
+                            glColor(radar.getEnemyContactColor(),
+                                    radar.getContactAlpha() * alphaMod, false);
+                        }
+                    }
+
+                    // Allied missiles
+                    else
+                    {
+                        glColor(radar.getFriendlyContactColor(),
+                                radar.getContactAlpha() * alphaMod, false);
+                    }
+
+                    Vector2f radarLoc = radar.getPointOnRadar(missile.getLocation());
+                    glVertex2f(radarLoc.x, radarLoc.y);
+                }
+                glEnd();
+
+                if (SHOW_MISSILE_LOCK_ICON)
+                {
+                    // TODO
+                }
+            }
+        }
     }
 }
