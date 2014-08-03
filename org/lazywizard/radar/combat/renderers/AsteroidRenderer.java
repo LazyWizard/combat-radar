be3141d88e4b1d559cb79e9218d72fb30cde9940
LazyWizard <iu65a@hotmail.com>
2014-08-03 08:31:02 -0500

    (experimental) Switched AsteroidRenderer to use glDrawArrays()

--- org/lazywizard/radar/combat/renderers/AsteroidRenderer.java
+++ org/lazywizard/radar/combat/renderers/AsteroidRenderer.java
@@ -1,6 +1,8 @@
 package org.lazywizard.radar.combat.renderers;
 
 import java.awt.Color;
+import java.nio.FloatBuffer;
+import java.util.Iterator;
 import java.util.List;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.CombatEntityAPI;
@@ -10,6 +12,7 @@ import org.json.JSONObject;
 import org.lazywizard.lazylib.JSONUtils;
 import org.lazywizard.radar.combat.CombatRadar;
 import org.lazywizard.radar.combat.CombatRenderer;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.util.vector.Vector2f;
 import static org.lazywizard.lazylib.opengl.ColorUtils.glColor;
 import static org.lwjgl.opengl.GL11.*;
@@ -46,17 +49,29 @@ public class AsteroidRenderer implements CombatRenderer
             {
                 radar.enableStencilTest();
 
-                Vector2f radarLoc;
+                // Calculate raw vertices
+                float[] vertices = new float[asteroids.size() * 2];
+                Iterator<? extends CombatEntityAPI> iter = asteroids.iterator();
+                for (int v = 0; v < asteroids.size() * 2; v += 2)
+                {
+                    CombatEntityAPI asteroid = iter.next();
+                    Vector2f radarLoc = radar.getPointOnRadar(asteroid.getLocation());
+                    vertices[v] = radarLoc.x;
+                    vertices[v + 1] = radarLoc.y;
+                }
+
+                // Generate vertex map
+                FloatBuffer vertexMap = BufferUtils.createFloatBuffer(vertices.length).put(vertices);
+                vertexMap.flip();
+
+                // Draw asteroids
                 glColor(ASTEROID_COLOR, radar.getContactAlpha(), false);
                 glPointSize(2f * radar.getCurrentZoomLevel());
-                glBegin(GL_POINTS);
-                for (CombatEntityAPI asteroid : asteroids)
-                {
-                    radarLoc = radar.getPointOnRadar(asteroid.getLocation());
-                    glVertex2f(radarLoc.x, radarLoc.y);
-                }
-                glEnd();
-                
+                glEnableClientState(GL_VERTEX_ARRAY);
+                glVertexPointer(2, 0, vertexMap);
+                glDrawArrays(GL_POINTS, 0, vertices.length / 2);
+                glDisableClientState(GL_VERTEX_ARRAY);
+
                 radar.disableStencilTest();
             }
         }
