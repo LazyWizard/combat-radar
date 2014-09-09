c2ee0d6d07758b0b9add62e1d15c4a47353f7768
Robert Maguire <LazyWizard@users.noreply.github.com>
2014-09-08 23:35:38 -0500

    Implemented basic fleet contact renderer

--- org/lazywizard/radar/renderers/campaign/FleetRenderer.java
+++ org/lazywizard/radar/renderers/campaign/FleetRenderer.java
@@ -1,16 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.lazywizard.radar.renderers.campaign;
 
-/**
- *
- * @author LazyWizard
- */
-public class FleetRenderer
-{
+import java.util.List;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.lazywizard.lazylib.VectorUtils;
+import org.lazywizard.lazylib.opengl.DrawUtils;
+import org.lazywizard.radar.CampaignRadar;
+import org.lazywizard.radar.renderers.CampaignRenderer;
+import org.lwjgl.util.vector.Vector2f;
+import static org.lazywizard.lazylib.opengl.ColorUtils.glColor;
+import static org.lwjgl.opengl.GL11.glLineWidth;
 
+public class FleetRenderer implements CampaignRenderer
+{
+    private static boolean SHOW_FLEETS;
+    private CampaignRadar radar;
+
+    @Override
+    public void reloadSettings(JSONObject settings) throws JSONException
+    {
+        SHOW_FLEETS = settings.getBoolean("showFleets");
+    }
+
+    @Override
+    public void init(CampaignRadar radar)
+    {
+        this.radar = radar;
+    }
+
+    @Override
+    public void render(CampaignFleetAPI player, float amount)
+    {
+        if (SHOW_FLEETS)
+        {
+            List<CampaignFleetAPI> fleets = radar.filterVisible(
+                    player.getContainingLocation().getFleets(), 1_000);
+            if (!fleets.isEmpty())
+            {
+                radar.enableStencilTest();
+
+                glLineWidth(1f);
+                for (CampaignFleetAPI fleet : fleets)
+                {
+                    // Calculate color of fleet
+                    float relationship = fleet.getFaction().getRelationship(
+                            player.getFaction().getId());
+                    if (relationship < 0)
+                    {
+                        glColor(radar.getEnemyContactColor(), radar.getContactAlpha(), false);
+                    }
+                    else
+                    {
+                        glColor(radar.getFriendlyContactColor(), radar.getContactAlpha(), false);
+                    }
+
+                    // Draw fleet on radar (circle with angled triangle inside)
+                    Vector2f center = radar.getPointOnRadar(fleet.getLocation());
+                    float size = Math.max(60f, fleet.getRadius())
+                            * radar.getCurrentPixelsPerSU();
+                    size *= 2f; // Scale upwards for better visibility
+                    //DrawUtils.drawCircle(center.x, center.y, size, 16, false);
+                    DrawUtils.drawEllipse(center.x, center.y, size, size * .65f,
+                            VectorUtils.getFacing(fleet.getVelocity()), 3, true);
+                }
+
+                radar.disableStencilTest();
+            }
+        }
+    }
 }
