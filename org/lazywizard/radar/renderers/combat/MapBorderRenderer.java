acd70543227c99e4f8f35a15c9e12303b7f8c06b
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-03-20 08:30:41 -0500

    More work on MapBorderRenderer

--- org/lazywizard/radar/renderers/combat/MapBorderRenderer.java
+++ org/lazywizard/radar/renderers/combat/MapBorderRenderer.java
@@ -6,6 +6,7 @@ import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.lazywizard.lazylib.JSONUtils;
 import org.lazywizard.radar.CombatRadar;
 import org.lazywizard.radar.renderers.CombatRenderer;
 import org.lwjgl.util.vector.Vector2f;
@@ -15,6 +16,7 @@ import static org.lwjgl.opengl.GL11.*;
 public class MapBorderRenderer implements CombatRenderer
 {
     private static boolean SHOW_BORDERS;
+    private static Color RETREAT_AREA_COLOR, GRAVITY_BARRIER_COLOR;
     private float mapWidth, mapHeight;
     private CombatRadar radar;
 
@@ -22,6 +24,10 @@ public class MapBorderRenderer implements CombatRenderer
     public void reloadSettings(JSONObject settings) throws JSONException
     {
         SHOW_BORDERS = settings.getBoolean("showMapBorder");
+
+        settings = settings.getJSONObject("mapBorderRenderer");
+        RETREAT_AREA_COLOR = JSONUtils.toColor(settings.getJSONArray("retreatAreaColor"));
+        GRAVITY_BARRIER_COLOR = JSONUtils.toColor(settings.getJSONArray("gravityBarrierColor"));
     }
 
     @Override
@@ -45,19 +51,27 @@ public class MapBorderRenderer implements CombatRenderer
                     ur = radar.getPointOnRadar(new Vector2f(mapWidth, mapHeight));
             final float retreatDistance = 2000f * radar.getCurrentPixelsPerSU();
 
-            // Draw retreat area
-            glLineWidth(retreatDistance);
-            glColor(Color.YELLOW, 0.15f, false);
-            glBegin(GL_LINE_LOOP);
-            glVertex2f(ll.x + retreatDistance / 2f, ll.y + retreatDistance / 2f);
-            glVertex2f(ll.x + retreatDistance / 2f, ur.y - retreatDistance / 2f);
-            glVertex2f(ur.x - retreatDistance / 2f, ur.y - retreatDistance / 2f);
-            glVertex2f(ur.x - retreatDistance / 2f, ll.y + retreatDistance / 2f);
+            // Draw retreat areas
+            glColor(RETREAT_AREA_COLOR, radar.getRadarAlpha(), false);
+            glBegin(GL_QUADS);
+
+            // Player retreat area
+            glVertex2f(ll.x, ll.y);
+            glVertex2f(ll.x, ll.y + retreatDistance);
+            glVertex2f(ur.x, ll.y + retreatDistance);
+            glVertex2f(ur.x, ll.y);
+
+            // Enemy retreat area
+            glVertex2f(ll.x, ur.y);
+            glVertex2f(ll.x, ur.y - retreatDistance);
+            glVertex2f(ur.x, ur.y - retreatDistance);
+            glVertex2f(ur.x, ur.y);
             glEnd();
 
             // Draw gravity barrier
+            // FIXME: Doesn't quite line up with retreat area at max zoom
             glLineWidth(50f * radar.getCurrentPixelsPerSU());
-            glColor(Color.RED);
+            glColor(GRAVITY_BARRIER_COLOR, radar.getRadarAlpha(), false);
             glBegin(GL_LINE_LOOP);
             glVertex2f(ll.x, ll.y);
             glVertex2f(ll.x, ur.y);
