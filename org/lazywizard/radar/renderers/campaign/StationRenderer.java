0dbf13ee9db361d17de9a7d506e6cf3be56c2d40
LazyWizard <iu65a@hotmail.com>
2015-07-25 14:09:40 -0500

    Update StationRenderer and CommRelayRenderer to use isUpdateFrame

--- org/lazywizard/radar/renderers/campaign/StationRenderer.java
+++ org/lazywizard/radar/renderers/campaign/StationRenderer.java
@@ -1,5 +1,7 @@
 package org.lazywizard.radar.renderers.campaign;
 
+import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CampaignFleetAPI;
@@ -12,12 +14,13 @@ import org.lazywizard.radar.CommonRadar;
 import org.lazywizard.radar.renderers.CampaignRenderer;
 import static org.lwjgl.opengl.GL11.*;
 
-// TODO: Update to use isUpdateFrame
 public class StationRenderer implements CampaignRenderer
 {
     private static boolean SHOW_STATIONS;
+    private static int MAX_STATIONS_SHOWN;
     private static String STATION_ICON;
     private SpriteAPI icon;
+    private List<StationIcon> toDraw;
     private CommonRadar<SectorEntityToken> radar;
 
     @Override
@@ -28,63 +31,104 @@ public class StationRenderer implements CampaignRenderer
         settings = settings.getJSONObject("campaignRenderers")
                 .getJSONObject("stationRenderer");
         STATION_ICON = settings.optString("stationIcon", null);
+        MAX_STATIONS_SHOWN = settings.optInt("maxShown", 1_000);
     }
 
     @Override
     public void init(CommonRadar<SectorEntityToken> radar)
     {
-        this.radar = radar;
-
-        if (SHOW_STATIONS)
+        if (!SHOW_STATIONS)
         {
-            icon = Global.getSettings().getSprite("radar", STATION_ICON);
+            return;
         }
+
+        this.radar = radar;
+        icon = Global.getSettings().getSprite("radar", STATION_ICON);
+        toDraw = new ArrayList<>();
     }
 
     @Override
     public void render(CampaignFleetAPI player, float amount, boolean isUpdateFrame)
     {
-        if (SHOW_STATIONS)
+        if (!SHOW_STATIONS)
         {
-            List<SectorEntityToken> stations = radar.filterVisible(
+            return;
+        }
+
+        if (isUpdateFrame)
+        {
+            toDraw.clear();
+            final List<SectorEntityToken> stations = radar.filterVisible(
                     player.getContainingLocation().getEntitiesWithTag(
-                           Tags.STATION), 1_000);
+                            Tags.STATION), MAX_STATIONS_SHOWN);
             if (!stations.isEmpty())
             {
-                radar.enableStencilTest();
-                glEnable(GL_TEXTURE_2D);
-
-                icon.setAlphaMult(radar.getContactAlpha());
                 for (SectorEntityToken station : stations)
                 {
                     // Calculate color of station
-                    float relationship = station.getFaction().getRelationship(
+                    final float relationship = station.getFaction().getRelationship(
                             player.getFaction().getId());
+                    final Color color;
                     if (relationship < 0)
                     {
-                        icon.setColor(radar.getEnemyContactColor());
+                        color = radar.getEnemyContactColor();
                     }
                     else if (relationship > 0)
                     {
-                        icon.setColor(radar.getFriendlyContactColor());
+                        color = radar.getFriendlyContactColor();
                     }
                     else
                     {
-                        icon.setColor(radar.getNeutralContactColor());
+                        color = radar.getNeutralContactColor();
                     }
 
                     // Resize and draw station on radar
                     float[] center = radar.getRawPointOnRadar(station.getLocation());
                     float size = station.getRadius() * 2f * radar.getCurrentPixelsPerSU();
                     size *= 2f; // Scale upwards for better visibility
-                    icon.setSize(size, size);
-                    icon.setAlphaMult(radar.getContactAlpha());
-                    icon.renderAtCenter(center[0], center[1]);
+                    toDraw.add(new StationIcon(center[0], center[1], size, color));
                 }
-
-                glDisable(GL_TEXTURE_2D);
-                radar.disableStencilTest();
             }
+        }
+
+        // Don't draw if there's nothing to render!
+        if (toDraw.isEmpty())
+        {
+            return;
+        }
+
+        icon.setAlphaMult(radar.getContactAlpha());
+        radar.enableStencilTest();
+
+        // Draw all stations
+        glEnable(GL_TEXTURE_2D);
+        for (StationIcon sIcon : toDraw)
+        {
+            sIcon.render();
+        }
+        glDisable(GL_TEXTURE_2D);
+
+        radar.disableStencilTest();
+    }
+
+    private class StationIcon
+    {
+        private final float x, y, size;
+        private final Color color;
+
+        private StationIcon(float x, float y, float size, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.color = color;
+        }
+
+        private void render()
+        {
+            icon.setSize(size, size);
+            icon.setColor(color);
+            icon.renderAtCenter(x, y);
         }
     }
 }
