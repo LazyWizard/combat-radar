be3c237238f7265f41570bf8bb7571d7a5fa9088
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-08-05 11:46:55 -0500

    Renamed Slick2D package to avoid collisions with other mods that include it

--- radar/org/newdawn/slick/geom/package-info.java
+++ radar/org/newdawn/slick/geom/package-info.java
@@ -2,4 +2,4 @@
  * Contains polygon triangulation code taken from the Slick2D library. See
  * {@code license.txt} in the mod directory.
  */
-package org.newdawn.slick.geom;
+package radar.org.newdawn.slick.geom;
