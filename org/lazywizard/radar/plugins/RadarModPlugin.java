bfcbd9904a345234f58b82ad0a152a5c7e2381f0
LazyWizard <LazyWizard@localhost>
2015-09-19 19:16:47 -0500

    Added excluded_missiles.csv, can exclude decorative missiles by projectile ID

--- org/lazywizard/radar/plugins/RadarModPlugin.java
+++ org/lazywizard/radar/plugins/RadarModPlugin.java
@@ -1,24 +1,16 @@
 package org.lazywizard.radar.plugins;
 
-import java.io.IOException;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
-import org.json.JSONException;
 import org.lazywizard.radar.RadarSettings;
 
 // TODO: Unify combat and campaign codebases
-// TODO: Add csv to prevent certain ships from being drawn
 public class RadarModPlugin extends BaseModPlugin
 {
-    public static void reloadSettings() throws IOException, JSONException
-    {
-        RadarSettings.reloadSettings();
-    }
-
     @Override
     public void onApplicationLoad() throws Exception
     {
-        reloadSettings();
+        RadarSettings.reloadSettings();
     }
 
     @Override
