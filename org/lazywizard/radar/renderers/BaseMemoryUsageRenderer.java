01d9af5d348da6dccf5cf6d80baddb85e92a4dfc
Robert Maguire <Robert Maguire@localhost>
2015-12-24 05:50:32 -0600

    Finished MemoryUsageRenderer, shows heap and non-heap used/committed/max memory

--- org/lazywizard/radar/renderers/BaseMemoryUsageRenderer.java
+++ org/lazywizard/radar/renderers/BaseMemoryUsageRenderer.java
@@ -13,7 +13,9 @@ public abstract class BaseMemoryUsageRenderer
 {
     private MemoryMXBean memory;
     private Vector2f barLocation;
-    private float barWidth, barHeight, usedFraction = 0f, commitFraction = 0f;
+    private float barWidth, barHeight, halfWidth,
+            heapUsedFraction = 0f, heapCommitFraction = 0f,
+            nonHeapUsedFraction = 0f, nonHeapCommitFraction = 0f;
 
     protected void initiate(CommonRadar radar)
     {
@@ -21,9 +23,10 @@ public abstract class BaseMemoryUsageRenderer
         final float radarRadius = radar.getRenderRadius();
 
         // Location and size of bar on the screen
-        barWidth = radarRadius * .09f;
+        barWidth = radarRadius * .045f;
+        halfWidth = barWidth * 0.5f;
         barHeight = radarRadius * 2f;
-        barLocation = new Vector2f(radarCenter.x - (radarRadius * 1.15f) - barWidth,
+        barLocation = new Vector2f(radarCenter.x - (radarRadius * 1.15f) - (barWidth * 2f),
                 radarCenter.y - radarRadius);
 
         memory = ManagementFactory.getMemoryMXBean();
@@ -33,31 +36,63 @@ public abstract class BaseMemoryUsageRenderer
     {
         if (isUpdateFrame)
         {
-            final MemoryUsage heap = memory.getHeapMemoryUsage();
-            final double maxMemory = heap.getMax();
-            if (maxMemory == -1)
+            final MemoryUsage heap = memory.getHeapMemoryUsage(),
+                    nonHeap = memory.getNonHeapMemoryUsage();
+            final double maxHeapMemory = heap.getMax(), maxNonHeapMemory = nonHeap.getMax();
+            if (maxHeapMemory == -1)
             {
-                usedFraction = (float) (heap.getUsed() / (double) heap.getCommitted());
-                commitFraction = usedFraction;
+                heapUsedFraction = (float) (heap.getUsed() / (double) heap.getCommitted());
+                heapCommitFraction = 1f;
             }
             else
             {
-                usedFraction = (float) (heap.getUsed() / maxMemory);
-                commitFraction = (float) (heap.getCommitted() / maxMemory);
+                heapUsedFraction = (float) (heap.getUsed() / maxHeapMemory);
+                heapCommitFraction = (float) (heap.getCommitted() / maxHeapMemory);
+            }
+
+            if (maxNonHeapMemory == -1)
+            {
+                nonHeapUsedFraction = (float) (nonHeap.getUsed() / (double) nonHeap.getCommitted());
+                nonHeapCommitFraction = 1f;
+            }
+            else
+            {
+                nonHeapUsedFraction = (float) (nonHeap.getUsed() / maxNonHeapMemory);
+                nonHeapCommitFraction = (float) (nonHeap.getCommitted() / maxNonHeapMemory);
             }
         }
 
         glLineWidth(barWidth);
         glBegin(GL_LINES);
+
+        // Heap memory
+        // Used
         glColor(Color.WHITE);
-        glVertex2f(barLocation.x, barLocation.y);
-        glVertex2f(barLocation.x, barLocation.y + (barHeight * usedFraction));
+        glVertex2f(barLocation.x - halfWidth, barLocation.y);
+        glVertex2f(barLocation.x - halfWidth, barLocation.y + (barHeight * heapUsedFraction));
+        // Committed
         glColor(Color.GRAY);
-        glVertex2f(barLocation.x, barLocation.y + (barHeight * usedFraction));
-        glVertex2f(barLocation.x, barLocation.y + (barHeight * commitFraction));
+        glVertex2f(barLocation.x - halfWidth, barLocation.y + (barHeight * heapUsedFraction));
+        glVertex2f(barLocation.x - halfWidth, barLocation.y + (barHeight * heapCommitFraction));
+        // Unused, uncommitted
         glColor(Color.DARK_GRAY);
-        glVertex2f(barLocation.x, barLocation.y + (barHeight * commitFraction));
-        glVertex2f(barLocation.x, barLocation.y + barHeight);
+        glVertex2f(barLocation.x - halfWidth, barLocation.y + (barHeight * heapCommitFraction));
+        glVertex2f(barLocation.x - halfWidth, barLocation.y + barHeight);
+
+        // Non-heap memory
+        // Used
+        glColor(Color.WHITE);
+        glVertex2f(barLocation.x + halfWidth, barLocation.y);
+        glVertex2f(barLocation.x + halfWidth, barLocation.y + (barHeight * nonHeapUsedFraction));
+        // Committed
+        glColor(Color.GRAY);
+        glVertex2f(barLocation.x + halfWidth, barLocation.y + (barHeight * nonHeapUsedFraction));
+        glVertex2f(barLocation.x + halfWidth, barLocation.y + (barHeight * nonHeapCommitFraction));
+        // Unused, uncommitted
+        glColor(Color.DARK_GRAY);
+        glVertex2f(barLocation.x + halfWidth, barLocation.y + (barHeight * nonHeapCommitFraction));
+        glVertex2f(barLocation.x + halfWidth, barLocation.y + barHeight);
+
         glEnd();
     }
 }
