50140d3893fae572d952b15a145563e8a8e4048e
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-12-24 04:57:06 -0600

    Started work on MemoryUsageRenderer (will be disabled by default)

---
+++ org/lazywizard/radar/renderers/combat/MemoryUsageRenderer.java
@@ -0,0 +1,30 @@
+package org.lazywizard.radar.renderers.combat;
+
+import com.fs.starfarer.api.combat.CombatEntityAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.lazywizard.radar.CommonRadar;
+import org.lazywizard.radar.renderers.BaseMemoryUsageRenderer;
+import org.lazywizard.radar.renderers.CombatRenderer;
+
+public class MemoryUsageRenderer extends BaseMemoryUsageRenderer implements CombatRenderer
+{
+
+    @Override
+    public void reloadSettings(JSONObject settings) throws JSONException
+    {
+    }
+
+    @Override
+    public void init(CommonRadar<CombatEntityAPI> radar)
+    {
+        super.initiate(radar);
+    }
+
+    @Override
+    public void render(ShipAPI player, float amount, boolean isUpdateFrame)
+    {
+        super.render(isUpdateFrame);
+    }
+}
