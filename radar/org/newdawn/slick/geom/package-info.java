ad33d1afd08cc709dfd3ea6d934a7ae8838a7840
Robert Maguire <LazyWizard@users.noreply.github.com>
2016-11-02 08:19:32 -0500

    Remove now unused Slick2D polygon triangulation classes

--- radar/org/newdawn/slick/geom/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains modified polygon triangulation code taken from the Slick2D library,
- * see {@code license.txt} in the mod directory. Put in its own package to avoid
- * conflicts with any mods that include the library.
- */
-package radar.org.newdawn.slick.geom;
